0810a980acbb1cbfbad259a81f3ddd762e725e55
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-12-01 22:18:19 -0500

    train

---
+++ model_training_lambda/lambda_function.py
@@ -0,0 +1,55 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+import pandas as pd 
+from prophet import Prophet
+
+
+def lambda_handler(event, context):
+    ddb_client = boto3.client('dynamodb', region_name='us-east-2')
+    ddb = boto3.resource('dynamodb', region_name='us-east-2')
+    table = ddb.Table('disneyridetimes')
+
+    rides = ['Disney Festival of Fantasy Parade',
+            'The Barnstormer',
+            'Dumbo the Flying Elephant',
+            'Jungle Cruise',
+            'Peter Pan''s Flight',
+            'Haunted Mansion',
+            'Meet Cinderella and Elena at Princess Fairytale Hall',
+            'it''s a small world',
+            'Prince Charming Regal Carrousel',
+            '"Monsters, Inc. Laugh Floor"',
+            'Space Mountain',
+            'Mickey''s PhilharMagic',
+            'Tomorrowland Transit Authority PeopleMover',
+            'Mad Tea Party',
+            'Astro Orbiter',
+            'The Many Adventures of Winnie the Pooh',
+            'Tomorrowland Speedway',
+            'Meet Mickey Mouse at Town Square Theater',
+            'Splash Mountain',
+            'Pirates of the Caribbean',
+            'Big Thunder Mountain Railroad',
+            'Swiss Family Treehouse',
+            'Under the Sea - Journey of The Little Mermaid',
+            'Buzz Lightyear''s Space Ranger Spin',
+            'Meet Rapunzel and Tiana at Princess Fairytale Hall',
+            'The Magic Carpets of Aladdin']
+    
+    for ride in rides:
+        response = table.get_item(
+            Key={
+                'ride_name': ride
+            }
+        )
+
+        df = pd.read_json(response['Item'])
+        print(df)
+
+
+
+ # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Lambda successfully completed!')
+    }
