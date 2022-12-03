80dca56b30a1c23a33957250fedbffcd6ee8f72e
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-12-03 16:18:25 -0500

    put

--- prediction_lambda/app/lambda_function.py
+++ prediction_lambda/app/lambda_function.py
@@ -65,7 +65,8 @@ def lambda_handler(event, context):
                         "yhat": {'S' :str(row['yhat'])},
                         "ride_name": {'S' :row['ride_name']}
                     }
-                    dynamodb.put_item(TableName='disneyridepreds', Item=pred);
+                
+                dynamodb.put_item(TableName='disneyridepreds', Item=pred);
                     # pred_list.append(pred)
         except:
             continue
