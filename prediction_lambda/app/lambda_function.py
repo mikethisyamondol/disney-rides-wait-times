d0f9150bbb0f2d50bcb6322d17891a6021a403cd
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-12-03 14:22:51 -0500

    try

--- prediction_lambda/app/lambda_function.py
+++ prediction_lambda/app/lambda_function.py
@@ -30,35 +30,38 @@ def lambda_handler(event, context):
     # cwd = os.getcwd()
     # os.chdir('/tmp/') 
     for o in files_in_s3:
-        m = pickle.loads(s3_r.Bucket(bucket).Object(folder+'/'+o).get()['Body'].read())
-        # s3.download_file(bucket, folder+'/'+o, '/tmp/'+o)
-        # print(contents.decode("utf-8"))
-        # m = pickle.load(open(cwd+'/tmp/'+o,'rb'))
+        try:
+            m = pickle.loads(s3_r.Bucket(bucket).Object(folder+'/'+o).get()['Body'].read())
+            # s3.download_file(bucket, folder+'/'+o, '/tmp/'+o)
+            # print(contents.decode("utf-8"))
+            # m = pickle.load(open(cwd+'/tmp/'+o,'rb'))
 
-        for pred_date in date_list:
-            future_date = pd.DataFrame({'ds':[f'{pred_date} 8:00:00',
-                                                    f'{pred_date} 9:00:00',
-                                                    f'{pred_date} 10:00:00',
-                                                    f'{pred_date} 11:00:00',
-                                                    f'{pred_date} 12:00:00',
-                                                    f'{pred_date} 13:00:00',
-                                                    f'{pred_date} 14:00:00',
-                                                    f'{pred_date} 15:00:00',
-                                                    f'{pred_date} 16:00:00',
-                                                    f'{pred_date} 17:00:00',
-                                                    f'{pred_date} 18:00:00',
-                                                    f'{pred_date} 19:00:00',
-                                                    f'{pred_date} 20:00:00',
-                                                    f'{pred_date} 21:00:00',
-                                                    f'{pred_date} 22:00:00',]})
-            forecast = m.predict(future_date)
-            pred = {
-                        "ds": {'S': forecast['ds']},
-                        "yhat": {'S': forecast["yhat"]},
-                        "ride_name": {'S': o}
-                    }
-        
-            pred_list.append(pred)
+            for pred_date in date_list:
+                future_date = pd.DataFrame({'ds':[f'{pred_date} 8:00:00',
+                                                        f'{pred_date} 9:00:00',
+                                                        f'{pred_date} 10:00:00',
+                                                        f'{pred_date} 11:00:00',
+                                                        f'{pred_date} 12:00:00',
+                                                        f'{pred_date} 13:00:00',
+                                                        f'{pred_date} 14:00:00',
+                                                        f'{pred_date} 15:00:00',
+                                                        f'{pred_date} 16:00:00',
+                                                        f'{pred_date} 17:00:00',
+                                                        f'{pred_date} 18:00:00',
+                                                        f'{pred_date} 19:00:00',
+                                                        f'{pred_date} 20:00:00',
+                                                        f'{pred_date} 21:00:00',
+                                                        f'{pred_date} 22:00:00',]})
+                forecast = m.predict(future_date)
+                pred = {
+                            "ds": {'S': forecast['ds']},
+                            "yhat": {'S': forecast["yhat"]},
+                            "ride_name": {'S': o}
+                        }
+            
+                pred_list.append(pred)
+        except:
+            continue
     
     dynamodb.put_item(TableName='disneyridepreds', Item=pred_list)
 
