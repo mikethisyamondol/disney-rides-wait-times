13fabbbefc9aacf9d9073831a2c5bec3a31366f5
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-12-03 13:21:21 -0500

    build

--- prediction_lambda/app/lambda_function.py
+++ prediction_lambda/app/lambda_function.py
@@ -28,7 +28,7 @@ def lambda_handler(event, context):
     # print(result)
     # print(result.get('Contents'))
     # cwd = os.getcwd()
-    os.chdir('/tmp/')
+    os.chdir('/tmp/') 
     for o in files_in_s3:
         s3.download_file(bucket, folder+'/'+o, '/tmp/'+o)
         # print(contents.decode("utf-8"))
